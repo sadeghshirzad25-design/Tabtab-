fa4cf1ab290cb907e4b79a2b486038041e028c06
sadeghshirzad25-design <sadeghshirzad25@gmail.com>
2026-08-28 19:09:24 +0330

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+echo "=== شروع نصب طبّاب ==="
+npx create-next-app@latest tabtab --typescript --tailwind --eslint --app --src-dir --import-alias "@/*" --yes
+cd tabtab
+npm install @supabase/supabase-js @supabase/ssr
+echo "=== تمام شد! ==="
 
